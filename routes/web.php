2bfbda1d88f4211219d0316e11eee0cc0e6f17c2
Lauris <l.ikaunieks@gmail.com>
2022-06-07 10:59:33 +0300

    First attempt with git

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::controller(PostController::class)->group(function(){
+    Route::prefix('posts')->group(function(){
+        Route::get('/', 'index')->name('posts.index');
+        Route::get('/create', 'create');
+        Route::post('/create', 'store')->name('posts.create');
+        Route::get ('/show/{post}', 'show')->name('posts.show');
+        Route::get('/edit/{post}', 'edit')->name('post.edit');
+        Route::post('/edit/{post}', 'update');
+        Route::get('/delete/{post}', 'destroy')->name('posts.delete');
+    });
+});
